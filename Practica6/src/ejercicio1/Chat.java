5607e21aab24fd754c3602c8ef633654f978da4b
cacu2ggg <cacu2ggg@aab15767-b663-5941-4d29-33d1b6c9fcf9>
2014-03-11 15:02:34 +0000

    Ejercico1: Chat

---
+++ Practica6/src/ejercicio1/Chat.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Chat extends Remote {
+    String darseDeAlta() throws RemoteException;
+    String darseDeBaja(Integer id) throws RemoteException;
+    String difundir(String mensaje) throws RemoteException;
+}
