92dd6e9bc1f076f7a29a47fabb9b58e507c9b877
cacu2ggg <cacu2ggg@aab15767-b663-5941-4d29-33d1b6c9fcf9>
2014-06-06 18:44:07 +0000

    Todo fenomenal solo falta que haga bien el spawn de los broadcast

--- trunk/Practica7/servidorchat.erl
+++ trunk/Practica7/servidorchat.erl
@@ -1,50 +1,3 @@
-%%% Message passing utility.  
-%%% User interface:
-%%% login(Name)
-%%%     One user at a time can log in from each Erlang node in the
-%%%     system messenger: and choose a suitable Name. If the Name
-%%%     is already logged in at another node or if someone else is
-%%%     already logged in at the same node, login will be rejected
-%%%     with a suitable error message.
-%%% logoff()
-%%%     Logs off anybody at at node
-%%% message(ToName, Message)
-%%%     sends Message to ToName. Error messages if the user of this 
-%%%     function is not logged on or if ToName is not logged on at
-%%%     any node.
-%%%
-%%% One node in the network of Erlang nodes runs a server which maintains
-%%% data about the logged on users. The server is registered as "messenger"
-%%% Each node where there is a user logged on runs a client process registered
-%%% as "mess_client" 
-%%%
-%%% Protocol between the client processes and the server
-%%% ----------------------------------------------------
-%%% 
-%%% To server: {ClientPid, logon, UserName}
-%%% Reply {messenger, stop, user_exists_at_other_node} stops the client
-%%% Reply {messenger, logged_on} logon was successful
-%%%
-%%% When the client terminates for some reason
-%%% To server: {'EXIT', ClientPid, Reason}
-%%%
-%%% To server: {ClientPid, message_to, ToName, Message} send a message
-%%% Reply: {messenger, stop, you_are_not_logged_on} stops the client
-%%% Reply: {messenger, receiver_not_found} no user with this name logged on
-%%% Reply: {messenger, sent} Message has been sent (but no guarantee)
-%%%
-%%% To client: {message_from, Name, Message},
-%%%
-%%% Protocol between the "commands" and the client
-%%% ---------------------------------------------- 
-%%%
-%%% Started: messenger:client(Server_Node, Name)
-%%% To client: logoff
-%%% To client: {message_to, ToName, Message}
-%%%
-%%% Configuration: change the server_node() function to return the
-%%% name of the node where the messenger server runs
-
 -module(servidorchat).
 -export([start_server/0, server/0]).
 
@@ -64,12 +17,15 @@ server(User_List,Message_Counter) ->
             New_User_List = server_logoff(From, User_List),
             server(New_User_List,Message_Counter);
     	{From, message_all, Message} ->
-    	    [ToPid ! {server_transfer(From, To, Message, User_List)} || {ToPid, To} <- User_List],
             New_Message_Counter = incrementa(Message_Counter),
+            entregarMensaje(User_List,From,Message,New_Message_Counter),
+            
             io:format("Message_Counter is now: ~p~n", [New_Message_Counter]),
             server(User_List,New_Message_Counter)
     end.
 
+entregarMensaje(User_List,From,Message,Message_Counter) ->
+    [ToPid ! {server_transfer(From, To, Message,Message_Counter, User_List)} || {ToPid, To} <- User_List].
 incrementa(V) ->
     V + 1.
 
@@ -96,7 +52,7 @@ server_logoff(From, User_List) ->
     lists:keydelete(From, 1, User_List).
 
 %%% Server transfers a message between user
-server_transfer(From, To, Message, User_List) ->
+server_transfer(From, To, Message, Message_Counter, User_List) ->
     %% check that the user is logged on and who he is
     case lists:keysearch(From, 1, User_List) of
         false ->
@@ -104,19 +60,19 @@ server_transfer(From, To, Message, User_List) ->
         {value, {_, Name}} ->
             case string:equal(Name,To) of
                 false ->
-                    server_transfer(From, Name, To, Message, User_List);
+                    server_transfer(From, Name, To, Message, Message_Counter, User_List);
                 true ->
                     From ! {clientechat, sent}
             end
     end.
 
 %%% If the user exists, send the message
-server_transfer(From, Name, To, Message, User_List) ->
+server_transfer(From, Name, To, Message, Message_Counter, User_List) ->
     %% Find the receiver and send the message
     case lists:keysearch(To, 2, User_List) of
         false ->
             From ! {clientechat, receiver_not_found};
         {value, {ToPid, To}} ->
-            ToPid ! {message_from, Name, Message}, 
+            ToPid ! {message_from, Name, Message, Message_Counter}, 
             From ! {clientechat, sent}
     end.
